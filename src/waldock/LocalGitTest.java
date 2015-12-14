0a9147e71a23948c6d8f7dba53fa20f60d3cd0db
Kevin Waldock <kevin.waldock@gmail.com>
2015-12-14 11:42:06 +0100

    More Hello World!

--- src/waldock/LocalGitTest.java
+++ src/waldock/LocalGitTest.java
@@ -4,6 +4,9 @@ public class LocalGitTest {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("Hello World 2!");
+		System.out.println("Hello World 3!");
+		
 	}
 
 }
